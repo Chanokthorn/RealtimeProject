3fca06c8afe314794392dc3bb0c3ba348556b4d7
Chanokthorn Uerpairojkit <chankychanky@hotmail.com>
2018-04-15 15:55:10 +0700

    Project init

---
+++ app.js
@@ -0,0 +1,68 @@
+// ------------------------------------------------
+// BASIC SETUP
+// ------------------------------------------------
+// Create an empty scene
+
+var scene = new THREE.Scene();
+
+
+// Create a basic perspective camera
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+camera.position.z = 4;
+
+// Create a renderer with Antialiasing
+var renderer = new THREE.WebGLRenderer({antialias:true});
+
+// Configure renderer clear color
+renderer.setClearColor("#000000");
+
+// Configure renderer size
+renderer.setSize( window.innerWidth, window.innerHeight );
+
+// Append Renderer to DOM
+document.body.appendChild( renderer.domElement );
+
+// ------------------------------------------------
+// FUN STARTS HERE
+
+// ------------------------------------------------
+
+let xSpeed = 1;
+let ySpeed = 1;
+
+document.addEventListener("keydown", onDocumentKeyDown, false);
+function onDocumentKeyDown(event) {
+    var keyCode = event.which;
+    if (keyCode == 87) {
+        cube.position.y += ySpeed;
+    } else if (keyCode == 83) {
+        cube.position.y -= ySpeed;
+    } else if (keyCode == 65) {
+        cube.position.x -= xSpeed;
+    } else if (keyCode == 68) {
+        cube.position.x += xSpeed;
+    } else if (keyCode == 32) {
+        cube.position.set(0, 0, 0);
+    }
+};
+
+// Create a Cube Mesh with basic material
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+var material = new THREE.MeshBasicMaterial( { color: "#433F81" } );
+var cube = new THREE.Mesh( geometry, material );
+
+// Add cube to Scene
+scene.add( cube );
+
+// Render Loop
+var render = function () {
+  requestAnimationFrame( render );
+
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  // Render the scene
+  renderer.render(scene, camera);
+};
+
+render();
